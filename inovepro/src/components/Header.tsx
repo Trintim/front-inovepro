f25493ce33e6d42d81b02b8947a295c64dfa84d2
Victor Trintim <49760038+Trintim@users.noreply.github.com>
2022-06-23 17:26:02 -0300

    update

--- inovepro/src/components/Header.tsx
+++ inovepro/src/components/Header.tsx
@@ -35,10 +35,47 @@ export default function Header() {
           (scrollActive ? "shadow-md pt-0" : "pt-4")
         }
       >
-        <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto grid grid-flow-col py-2 sm:py-2">
-          <div className="col-start-1 col-end-2 flex items-center">
-            <Logo height={"64"} width={"64"} />
-          </div>
+        <nav className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto flex py-2 sm:py-2">
+          <Disclosure>
+            {({ open }) => (
+              <>
+                <div className="col-start-1 col-end-2 flex flex-grow items-center">
+                  <Logo height={"64"} width={"64"} />
+                </div>
+                <Disclosure.Button
+                  aria-label="Toggle Menu"
+                  className="col-end-11 flex lg:hidden justify-end items-center text-rose-500"
+                >
+                  {!open && MenuIcon(8)}
+
+                  {open && CloseIcon(8)}
+                </Disclosure.Button>
+                <Disclosure.Panel className="grid grid-flow-row w-full my-5 lg:hidden ">
+                  {navigation.map((item, index) => (
+                    <LinkScroll
+                      key={index}
+                      activeClass="active"
+                      to={`"${item}"`}
+                      spy={true}
+                      smooth={true}
+                      duration={1000}
+                      onSetActive={() => {
+                        setActiveLink(`"${item}"`);
+                      }}
+                      className={
+                        "px-4 py-2 mx-2 cursor-pointer animation-hover" +
+                        (activeLink === `"${item}"`
+                          ? " text-gray-200 animation-active"
+                          : " text-gray-200 hover:text-rose-500 bg-slate-500 a")
+                      }
+                    >
+                      {item}
+                    </LinkScroll>
+                  ))}
+                </Disclosure.Panel>
+              </>
+            )}
+          </Disclosure>
           <ul className="col-start-3 col-end-9 hidden lg:flex items-center justify-center">
             {navigation.map((item, index) => (
               <LinkScroll
@@ -65,46 +102,6 @@ export default function Header() {
           <div className="col-start-10 col-end-12 hidden lg:flex font-medium justify-end items-center">
             <ButtonNav>Contato</ButtonNav>
           </div>
-          <Disclosure>
-            {({ open }) => (
-              <>
-                <Disclosure.Button
-                  aria-label="Toggle Menu"
-                  className="col-end-11 flex lg:hidden justify-end items-center text-white"
-                >
-                  {!open && MenuIcon(8)}
-
-                  {open && CloseIcon(8)}
-                </Disclosure.Button>
-
-                <Disclosure.Panel>
-                  <ul className="container lg:hidden flex flex-col items-start justify-center">
-                    {navigation.map((item, index) => (
-                      <LinkScroll
-                        key={index}
-                        activeClass="active"
-                        to={item}
-                        spy={true}
-                        smooth={true}
-                        duration={1000}
-                        onSetActive={() => {
-                          setActiveLink({ item });
-                        }}
-                        className={
-                          "px-4 py-2 mx-2 cursor-pointer animation-hover " +
-                          (activeLink === { item }
-                            ? " text-gray-200 hover:bg-rose-600 animation-active "
-                            : " text-gray-200 hover:text-rose-500 a")
-                        }
-                      >
-                        {item}
-                      </LinkScroll>
-                    ))}
-                  </ul>
-                </Disclosure.Panel>
-              </>
-            )}
-          </Disclosure>
         </nav>
       </header>
     </>
